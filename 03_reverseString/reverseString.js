c2bd23180ccd8a8849edc389b69586cfa0f534fa
Aemilia <oxideseven@users.noreply.github.com>
2023-07-26 15:33:29 -0500

    simplifies function with array reverse method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,5 @@
 const reverseString = function (string) {
-    let revString='';
-    let long = string.length + 1;
-    
-    for (let i = 0; i < long; i++){
-         revString += string.slice(long - i - 1,long - i);
-     }
-     return(revString);
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
