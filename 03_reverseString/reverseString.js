e401332c775a40e59083dd8f89c8cbc80618c6ad
Aemilia <oxideseven@users.noreply.github.com>
2023-07-26 15:32:06 -0500

    adds test passing function code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let revString='';
+    let long = string.length + 1;
+    
+    for (let i = 0; i < long; i++){
+         revString += string.slice(long - i - 1,long - i);
+     }
+     return(revString);
 };
 
 // Do not edit below this line
