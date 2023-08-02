c5215e29636c96668e4e16243a882893376fb867
Aemilia <oxideseven@users.noreply.github.com>
2023-08-02 12:39:53 -0500

    add test passing code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+	
+	if ((typeof num1 !== "number") || (typeof num2 !== "number") || (num1 < 0) || (num2 < 0)){
+		return('ERROR');
+		
+	}else if (num1 < num2) {
+		for (let i = num1; i <= num2; i++){
+			sum += i;
+		}
+		return(sum);
+		
+	}else if (num2 < num1){
+		for (let i = num2; i <= num1; i++){
+			sum += i;
+		}
+	    return(sum);
+	}
 };
 
 // Do not edit below this line
