bbf9908780b712eb59cd9371edca2ddffb565ef9
Aemilia <oxideseven@users.noreply.github.com>
2023-08-11 16:30:43 -0500

    add test passing code.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
+const findTheOldest = function(peeps) {
+    
+    const currentYear = (new Date()).getFullYear();
 
+    return peeps.sort((a, b) => (((a.yearOfDeath) ||(currentYear )) - a.yearOfBirth) > (((b.yearOfDeath) || (currentYear)) - b.yearOfBirth) ? -1 : 1)[0];
 };
 
 // Do not edit below this line
