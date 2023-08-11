9e747a65819cca9d9e427d00e39202648ca530ee
Aemilia <oxideseven@users.noreply.github.com>
2023-08-11 15:14:51 -0500

    add test passing code.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(args) {
+    let titles = [];
+    for (let i = 0; i < args.length; i++){
+        titles.push(args[i].title)
+    }
+    return titles;
 };
 
 // Do not edit below this line
