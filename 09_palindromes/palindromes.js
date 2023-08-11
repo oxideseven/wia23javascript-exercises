3201ded5e5cad56829893decff806d2aef76be48
Aemilia <oxideseven@users.noreply.github.com>
2023-08-11 14:19:06 -0500

    add test passing code.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    
+    cleanString = string.toLowerCase().replace(/[^A-Z0-9]/ig,``)
+	reverseString = Array.from(cleanString).reverse().join(``);
+        
+    return(reverseString === cleanString);
+    
 };
 
 // Do not edit below this line
