d077d62b93291f37c0217f204e9f67b0c194cfea
Aemilia <oxideseven@users.noreply.github.com>
2023-08-11 15:03:11 -0500

    add test passing code.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(digit) {
+    let lastNum = 0;
+	let currentNum = 1;
+    let nextNum;
+    
+    if (digit < 0) {
+        return ("OOPS")
+    } else {
+        for (let i = 0; i < digit; i++) {
+            nextNum = lastNum + currentNum;
+            lastNum = currentNum;
+            currentNum = nextNum;
+        }
+        return lastNum;
+    }
 };
 
 // Do not edit below this line
